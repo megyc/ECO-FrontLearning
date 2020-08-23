6c1bceff424497bd6d6fe40da0965cf4e44587c6
GuoYC <gycchen@foxmail.com>
2020-08-23 21:40:26 +0800

    用复制的方法实现了语言切换的功能

---
+++ test/main_jps.js
@@ -0,0 +1,36 @@
+const login_button=document.getElementById("login-button");
+const chs_button=document.getElementById("chs");
+
+login_button.onclick=login;
+chs_button.onclick=jump_to_chs;
+
+let User="admin"
+let PassWord="123"
+
+function jump_to_chs(){
+    console.info("test");
+    window.location="index.html";
+}
+
+function login(){
+    let number=document.getElementById("input1").value;
+    let password=document.getElementById("input2").value;
+    let number_text;
+    //let psssword_text;
+    //非空校验
+    number_text=number.split(" ").join("");//去除用户名中的空格，密码先不考虑
+    if(number_text===""){
+        alert("工号不能为空");
+        document.getElementById("input1").value="";
+    }
+    else if(password===""){
+        alert("密码不能为空");
+    }
+    else if(User===number_text&&PassWord===password){
+        alert("登陆成功");
+    }
+    else{
+        alert("用户名或密码错误");
+        document.getElementById("input2").value="";
+    }
+}
