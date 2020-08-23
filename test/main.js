440fc988bd2b575f0e93d324d01e2ff8e4c9089f
GuoYC <gycchen@foxmail.com>
2020-08-23 16:35:37 +0800

    添加了简单的按钮监听

---
+++ test/main.js
@@ -0,0 +1,10 @@
+const login_button=document.getElementById("login-button");
+
+login_button.onclick=login;
+
+function login(){
+    let number=document.getElementById("input1");
+    let password=document.getElementById("input2");
+    console.log(number);
+
+}
